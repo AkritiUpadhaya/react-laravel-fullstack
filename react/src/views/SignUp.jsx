bee8f2d96089dd4886f834c485d69e736e489b5b
Akriti <upadhayaakriti23@gmail.com>
2024-10-06 22:50:12 +0545

    wip-signup created

--- react/src/views/SignUp.jsx
+++ react/src/views/SignUp.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function SignUp() {
+  const onSubmit=(e)=>{
+   e.preventDefault()
+  }
   return (
-    <div>
-      sign Up
+    <div className='login-signup-form animated fadeInDown'>
+      <div className="form">
+        <form onSubmit={onSubmit}>
+          <h1 className='title'>Sign up for free</h1>
+          <input type='text' placeholder='Full Name' />
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+          <input type='password' placeholder='Confirm Password' />
+          <button className='btn btn-block'>Sign up</button>
+          <p className='message'>
+            Already Registered? <Link to="/Login">Sign in</Link>
+            </p> 
+        </form>
+      </div>
     </div>
   )
 }
